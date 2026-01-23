f96b6d1eef0eed3c741fef370885528ec00e985d
KevinS <kevin.saison91@gmail.com>
2026-01-23 08:15:56 +0100

    Implementation of a template for the projects page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "pages/profil"
   get "pages/skills"
   get "home/index"
+  get "projects/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
