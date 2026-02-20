906d017d7f7ae79f58a1ee2e58f922ac8c5abf26
KevinS <kevin.saison91@gmail.com>
2026-02-20 12:25:40 +0100

    Improve performances

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module KsPortfolio
   class Application < Rails::Application
+    config.middleware.use Rack::Deflater
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
