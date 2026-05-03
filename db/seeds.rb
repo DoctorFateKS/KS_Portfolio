08b245cf771aa74484407a92eb04464611a58351
KevinS <kevin.saison91@gmail.com>
2026-05-03 15:22:23 +0200

    Project page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,39 @@
 Project.destroy_all
 
 Project.create(
-  title: "Sharefriends 1",
-  role_jp: "主任開発者",
-  role_description: "Lead Developer",
-  github_url: "https://github.com/DoctorFateKS/Sharefriends",
+  title: "SkillEngine (en cours de développement)",
+  role_jp: "フリーランス",
+  role_description: "Freelance",
+  github_url: "https://github.com/DoctorFateKS/SkillEn",
   demo_url: "#",
-  tagline: "Sharefriends is a web application to facilitate friendship-building through shared affinities.",
-  challenges: "Interest-Based Matching – An algorithm that suggests profiles based on a shared list of passions.",
-  stack: "Ruby on Rails, PostgreSQL, ActionCable, Stimulus JS, RSpec",
-  results: "Interest-Based Matching, Geolocated Discovery, Real-Time Messaging, Dynamic Profile Management, Local Events"
+  tagline: "SaaS visant à faciliter la recherche d’emploi dans la tech en récupérant les skills dans les offres d’emplois pour les comparer à ceux des utilisateurs et offrir un score de compatibilité.",
+  challenges: "Ambition de devenir l’outil référence de la recherche d’emploi en France.",
+  stack: "Ruby, Ruby on Rails, API France Travail, Natural Language Processing",
+  results: "Bientôt en production"
 )
 
 Project.create(
-  title: "Sharefriends 2",
-  role_jp: "主任開発者",
-  role_description: "Lead Developer",
-  github_url: "https://github.com/DoctorFateKS/Sharefriends",
-  demo_url: "#",
-  tagline: "Sharefriends is a web application to facilitate friendship-building through shared affinities.",
-  challenges: "Interest-Based Matching – An algorithm that suggests profiles based on a shared list of passions.",
-  stack: "Ruby on Rails, PostgreSQL, ActionCable, Stimulus JS, RSpec",
-  results: "Interest-Based Matching, Geolocated Discovery, Real-Time Messaging, Dynamic Profile Management, Local Events"
+  title: "KS Portfolio",
+  role_jp: "個人プロジェクト",
+  role_description: "Projet Personnel",
+  github_url: "https://github.com/DoctorFateKS/KS_Portfolio",
+  demo_url: "https://kevinsaison.com/",
+  tagline: "Application Ruby on Rails pour mettre en avant mes compétences, mes projets et mes écrits.",
+  challenges: "Augmentation des prises de contact et de la visibilité.",
+  stack: "Ruby, Ruby on Rails, Kamal, Docker, Serveur Hetzner",
+  results: "Skills, Projets, Blog"
 )
 
 Project.create(
-  title: "Sharefriends 3",
+  title: "Sharefriends",
   role_jp: "主任開発者",
   role_description: "Lead Developer",
   github_url: "https://github.com/DoctorFateKS/Sharefriends",
   demo_url: "#",
-  tagline: "Sharefriends is a web application to facilitate friendship-building through shared affinities.",
-  challenges: "Interest-Based Matching – An algorithm that suggests profiles based on a shared list of passions.",
-  stack: "Ruby on Rails, PostgreSQL, ActionCable, Stimulus JS, RSpec",
-  results: "Interest-Based Matching, Geolocated Discovery, Real-Time Messaging, Dynamic Profile Management, Local Events"
-)
-
-Project.create(
-  title: "Sharefriends 4",
-  role_jp: "主任開発者",
-  role_description: "Lead Developer",
-  github_url: "https://github.com/DoctorFateKS/Sharefriends",
-  demo_url: "#",
-  tagline: "Sharefriends is a web application to facilitate friendship-building through shared affinities.",
-  challenges: "Interest-Based Matching – An algorithm that suggests profiles based on a shared list of passions.",
-  stack: "Ruby on Rails, PostgreSQL, ActionCable, Stimulus JS, RSpec",
-  results: "Interest-Based Matching, Geolocated Discovery, Real-Time Messaging, Dynamic Profile Management, Local Events"
+  tagline: "Sharefriends est une application Ruby on Rails de rencontre amical basée sur les centres d’intérêts communs.",
+  challenges: "Matching par centre d'intérêt – Un algorithme qui suggère des activités et des profils en fonction des passions",
+  stack: "Ruby, Ruby on Rails, Hotwire, ActionCable",
+  results: "Matching par centre d'intérêt, Messagerie temps-réél"
 )
 
 puts "Projets créé !"
